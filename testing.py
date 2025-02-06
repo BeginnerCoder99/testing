451cef2c9ef69b78fb5e6579fa7b4f3428106549
anthony tony <anthony.ellison120@yahoo.com>
2025-02-06 16:17:56 -0600

    updating python file

--- testing.py
+++ testing.py
@@ -1,2 +1,16 @@
-#testing
-#testing2
+#Just a copy of decorator example in class
+
+def add_method(cls):
+    class NewClass(cls):
+            def new_method(self):
+                  print("This is the new one")
+    return NewClass
+@add_method
+class MyClass:
+      def original_method(self):
+            print("This is the original")
+
+
+obj = MyClass()
+obj.original_method()
+obj.new_method()
